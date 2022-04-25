76af9fc51096c4844ee60982659bcc31dbcba6d1
softtech <softtech@192.168.1.6>
2022-04-25 14:14:55 +0900

    契約情報

---
+++ src/main/java/com/softtech/controller/ContractInfoController.java
@@ -0,0 +1,25 @@
+package com.softtech.controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * 概要：契約情報リスト初期表示
+ *
+ * 作成者：テー@ソフトテク
+ * 作成日：2022/4/25
+ *
+ */
+@Controller
+public class ContractInfoController {
+	@RequestMapping("/initContractInfoList")
+	public String toContractInfoList(Model model, String contractInfoList) {
+		return contractInfoList;
+
+
+}
+}
