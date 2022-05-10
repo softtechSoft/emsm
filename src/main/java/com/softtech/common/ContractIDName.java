a2f32a9c1bba7c430571a425f31816c0df059040
softtech <softtech@192.168.1.6>
2022-05-10 15:11:25 +0900

    契約情報更新

---
+++ src/main/java/com/softtech/common/ContractIDName.java
@@ -0,0 +1,33 @@
+package com.softtech.common;
+
+public class ContractIDName {
+	// ユーザーID
+	private String ContractID;
+	// ユーザー名称
+	private String ContractName;
+	/**
+	 * @return contractID
+	 */
+	public String getContractID() {
+		return ContractID;
+	}
+	/**
+	 * @param contractID セットする contractID
+	 */
+	public void setContractID(String contractID) {
+		ContractID = contractID;
+	}
+	/**
+	 * @return contractName
+	 */
+	public String getContractName() {
+		return ContractName;
+	}
+	/**
+	 * @param contractName セットする contractName
+	 */
+	public void setContractName(String contractName) {
+		ContractName = contractName;
+	}
+
+}
