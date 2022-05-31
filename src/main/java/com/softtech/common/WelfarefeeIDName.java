ddc5bc07d9a2742d02cf739ce805d8c76cfb46a0
SunYe <sonnyou251@gmail.com>
2022-05-31 09:08:24 +0900

    修正２

---
+++ src/main/java/com/softtech/common/WelfarefeeIDName.java
@@ -0,0 +1,18 @@
+package com.softtech.common;
+
+/**
+ * @program @概要:更新画面への用 @作成者:孫曄 @作成日:2022-05-24
+ * @return:
+ */
+public class WelfarefeeIDName {
+  // 厚生保険料ID PK
+  private String welfarefeeID;
+
+  public String getWelfarefeeID() {
+    return welfarefeeID;
+  }
+
+  public void setWelfarefeeID(String welfarefeeID) {
+    this.welfarefeeID = welfarefeeID;
+  }
+}
---
+++ src/main/java/com/softtech/mappers/WelfarefeeInfoMapper.java
@@ -0,0 +1,37 @@
+package com.softtech.mappers;
+
+import com.softtech.common.WelfarefeeIDName;
+import com.softtech.entity.WelfarefeeInfo;
+import com.softtech.entity.WelfarefeeInfoEntity;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @program @概要:厚生保険料データ画面の更新、新規追加 @作成者:孫曄 @作成日:2022-05-24
+ * @return:
+ */
+@Mapper
+public interface WelfarefeeInfoMapper {
+  // 画面表示用のquery,入力の収入により、検索する、保険料を納付したいとき、検索用
+  List<WelfarefeeInfoEntity> getWelfarefeeInfoByEnterSalary(
+      @Param("enterSalary") String enterSalary);
+  // 画面表示用のquery方法２：年度により、検索する
+  List<WelfarefeeInfoEntity> getWelfarefeeInfoByYear(@Param("year") String year);
+
+  // 更新画面への検索,表示される画面の厚生保険料IDにより、検索する
+  List<WelfarefeeInfoEntity> getUpdateWelfarefeeInfoList(String welfarefeeID);
+
+  // update
+  void updateWelfarefeeInfo(WelfarefeeInfoEntity welfarefeeInfoEntity);
+
+  // 最大のwelfarefeeIDを探す
+  String getMaxWelfarefeeID();
+
+  // insert
+  void insertWelfarefeeInfo(WelfarefeeInfoEntity welfarefeeInfoEntity);
+
+  // 初期表示画面を検索
+  List<WelfarefeeIDName> getWelfarefeeInfoID();
+}
