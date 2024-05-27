48839ce432f57368785507931adfd6e0392b101c
nguyenhung <nguyenhungkols>
2024-05-27 09:13:46 +0900

    junit test

--- src/test/java/com/softtech/service/EmployeeInfoServiceImplTest.java
+++ src/test/java/com/softtech/service/EmployeeInfoServiceImplTest.java
@@ -1,115 +1,41 @@
 package com.softtech.service;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.junit.jupiter.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 
-import com.softtech.entity.EmployeeInfoEntity;
 import com.softtech.mappers.EmployeeInfoMapper;
 
+public class EmployeeInfoServiceImplTest {
 
-class EmployeeInfoServiceImplTest {
+    @InjectMocks
+    private EmployeeInfoServiceImpl target;
 
-	@InjectMocks
-	private EmployeeInfoServiceImpl target;
+    @Mock
+    private EmployeeInfoMapper employeeInfoMapper;
 
-	@Mock
-	private EmployeeInfoMapper employeeInfoMapper;
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
 
-	@Mock
-	private EmployeeInfoEntity employeeInfoEntity;
 
-	@Spy
-	List<EmployeeInfoEntity> employeesPara = new ArrayList<>();
-	List<EmployeeInfoEntity> employeesName = new ArrayList<>();
-	List<EmployeeInfoEntity> employeesRtn =  new ArrayList<>();
 
-	@BeforeEach
-	void setUp() {
-		MockitoAnnotations.openMocks(this);
-	}
+    @Test
+    public void testGetNextEmployeeID() {
+        // モック
+        Mockito.when(employeeInfoMapper.getMaxEmployeeID()).thenReturn("E0001");
 
-	@Test
-	void changeSexTest() {
-		Mockito.when(changeSex(employeeInfoMapper.getEmployeeAll()));
-		List<EmployeeInfoEntity> employeeInfoEntity=new ArrayList<>();
-		EmployeeInfoEntity employeeInfoEntity1 = new EmployeeInfoEntity();
-		employeeInfoEntity1.setSex("1");
-		employeeInfoEntity.add(employeeInfoEntity1);
+        // 実行
+        String result = target.getNextEmployeeID();
 
-		EmployeeInfoEntity employeeInfoEntity2 = new EmployeeInfoEntity();
-		employeeInfoEntity2.setSex("0");
-		employeeInfoEntity.add(employeeInfoEntity2);
-
-		List<EmployeeInfoEntity> employeeInfoEntityRtn = target.changeSex(employeeInfoEntity);
-
-		EmployeeInfoEntity employeeInfoEntityrtn1 = new EmployeeInfoEntity();
-		EmployeeInfoEntity employeeInfoEntityrtn2 = new EmployeeInfoEntity();
-		int i = 0;
-		for(EmployeeInfoEntity rtn:employeeInfoEntityRtn) {
-			if (i==0) {
-				employeeInfoEntityrtn1=rtn;
-			} else {
-
-				employeeInfoEntityrtn2=rtn;
-			}
-			i++;
-		}
-
-		Assertions.assertEquals(employeeInfoEntityrtn1.getSex(),"男");
-		Assertions.assertEquals(employeeInfoEntityrtn2.getSex(),"女");
-	}
-
-	private Object changeSex(List<EmployeeInfoEntity> employeeAll) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
-	}
-
-	@Test
-	void getEmployeeAllTest() {
-//		 パラメータの準備
-		EmployeeInfoEntity employeeInfoEntity1 = new EmployeeInfoEntity();
-		employeeInfoEntity1.setSex("1");
-		employeeInfoEntity1.setEmployeeName("あいう");
-		employeeInfoEntity1.setAddress("Tokyo");
-		employeesPara.add(employeeInfoEntity1);
-
-		EmployeeInfoEntity employeeInfoEntity2 = new EmployeeInfoEntity();
-		employeeInfoEntity2.setSex("0");
-		employeeInfoEntity2.setEmployeeName("かきく");
-		employeeInfoEntity2.setAddress("Osaka");
-		employeesPara.add(employeeInfoEntity2);
-
-//		対象メソッドを呼び出し
-		Mockito.when(employeeInfoMapper.getEmployeeAll()).thenReturn(employeesPara);
-		List<EmployeeInfoEntity> employeesRtn=target.getEmployeeAll();
-
-//		対象データ確認
-		EmployeeInfoEntity employeeInfoEntityrtn1 = new EmployeeInfoEntity();
-		EmployeeInfoEntity employeeInfoEntityrtn2 = new EmployeeInfoEntity();
-		int i = 0;
-		for(EmployeeInfoEntity rtn:employeesRtn) {
-			if (i==0) {
-				employeeInfoEntityrtn1=rtn;
-			} else {
-
-				employeeInfoEntityrtn2=rtn;
-			}
-			i++;
-		}
-		Assertions.assertEquals(employeeInfoEntityrtn1.getSex(),"男");
-		Assertions.assertEquals(employeeInfoEntityrtn1.getEmployeeName(),"あいう");
-		Assertions.assertEquals(employeeInfoEntityrtn1.getAddress(),"Tokyo");
-		Assertions.assertEquals(employeeInfoEntityrtn2.getSex(),"女");
-		Assertions.assertEquals(employeeInfoEntityrtn2.getEmployeeName(),"かきく");
-		Assertions.assertEquals(employeeInfoEntityrtn2.getAddress(),"Osaka");
-	}
+        // 結果確認
+        assertThat(result).isEqualTo("E00002");
+    }
 }
 
