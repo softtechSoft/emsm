dd3df57b7fac17872df8906c61849fa1a394a3fa
yangxiwen <xiwen.yang@pactera.com>
2024-12-11 11:36:21 +0900

    年末調整テーブル改修

--- DOC/DB編/CreateTable.sql
+++ DOC/DB編/CreateTable.sql
@@ -576,41 +576,13 @@ END
 
 --新規年末調整テーブル
 
-CREATE TABLE ems.adjustmentRequest (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `employeeID` VARCHAR(6) NOT NULL,
-    `employeeEmail` VARCHAR(255) NOT NULL,
-    `year` INT NOT NULL,
-    `insertDate` DATE NOT NULL,
-    `updateDate` DATE NOT NULL,
-    FOREIGN KEY (`employeeID`) REFERENCES `ems`.`employee`(`employeeID`) ON DELETE CASCADE,
-    INDEX(`year`, `employeeID`, `employeeEmail`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-CREATE TABLE ems.adjustmentRequestFiles (
-    `fileID` INT AUTO_INCREMENT PRIMARY KEY,
-    `employeeID` VARCHAR(6),
-    `employeeEmail` VARCHAR(255) NOT NULL,
-    `fileName` VARCHAR(255) NOT NULL,
-    `fileYear` INT NOT NULL,
-    `fileULStatus` VARCHAR(50) NOT NULL,
-    `fileInsertDate` DATE NOT NULL,
-    `fileUpdateDate` DATE NOT NULL,
-    INDEX `idx_employee_id` (`employeeID`),
-    INDEX `idx_employee_email` (`employeeEmail`),
-    INDEX(`fileYear`, `fileName`),
-    FOREIGN KEY (`employeeID`) REFERENCES `ems`.`employee`(`employeeID`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-CREATE TABLE ems.adjustmentDetail(
+CREATE TABLE ems.adjustmentDetail (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `employeeID` VARCHAR(6) NOT NULL,
     `employeeEmail` VARCHAR(255) NOT NULL,
     `year` VARCHAR(4) NOT NULL,
     `uploadStatus` VARCHAR(50),
-    `adjustmentStatus` VARCHAR(50),
+    `adjustmentStatus` VARCHAR(50) DEFAULT '0',
     `insertDate` DATETIME,
     `updateDate` DATETIME,
     FOREIGN KEY (`employeeID`) REFERENCES `employee`(`employeeID`) ON DELETE CASCADE,
@@ -618,26 +590,35 @@ CREATE TABLE ems.adjustmentDetail(
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE ems.adjustmentFile (
-    `fileID` INT AUTO_INCREMENT PRIMARY KEY,
-    `employeeID` VARCHAR(6) NOT NULL,
-    `employeeEmail` VARCHAR(255) NOT NULL,
+    `employeeID` VARCHAR(6) NOT NULL PRIMARY KEY,
     `fileType` VARCHAR(255) NOT NULL,
     `fileYear` INT NOT NULL,
     `fileName` VARCHAR(255) NOT NULL,
+    `fileStatus` VARCHAR(50) NOT NULL,
     `filePath` VARCHAR(255) NOT NULL,
     FOREIGN KEY (`employeeID`) REFERENCES `employee`(`employeeID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
+CREATE TABLE ems.adjustmentRequestFiles (
+    `fileName` VARCHAR(255) NOT NULL,
+    `fileYear` INT NOT NULL,
+    `fileULStatus` VARCHAR(50) NOT NULL DEFAULT '0',
+    `filePath` VARCHAR(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `adjustmentDetail`
-MODIFY COLUMN `adjustmentStatus` VARCHAR(50) DEFAULT '0:未調整';
 
-ALTER TABLE ems.adjustmentRequestFiles
-ADD CONSTRAINT uniq_employee_file UNIQUE (employeeID, fileYear, fileName);
 
-ALTER TABLE ems.adjustmentFile
-ADD CONSTRAINT uniq_employee_file UNIQUE (employeeID, fileYear, fileName);
+ALTER TABLE ems.adjustmentFile 
+ADD CONSTRAINT uniq_employee_file 
+UNIQUE (employeeID, fileYear, fileName, fileType);
+
+
+ALTER TABLE ems.adjustmentFile 
+DROP PRIMARY KEY,
+ADD PRIMARY KEY (employeeID, fileType, fileYear, fileName);
+
+
 
 
 
