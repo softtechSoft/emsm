c7b65c18e1a77997d58ac9ed6fa2771e54e12c0e
Jason <xuexijava@springboot.com>
2025-07-23 09:06:54 +0900

    ｄｐ

--- src/main/java/com/softtech/EmsmApplication.java
+++ src/main/java/com/softtech/EmsmApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-public class EmsmApplication  extends SpringBootServletInitializer{
+@MapperScan("com.softtech.mappers")
+public class EmsmApplication extends SpringBootServletInitializer {
 	  @Override
 	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	        return application.sources(EmsmApplication.class);
